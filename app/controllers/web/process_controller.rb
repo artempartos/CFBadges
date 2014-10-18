b04b38f6bb40c97105c4bf97866e7470d7c2ac1d
Artem Petrov <partos0511@gmail.com>
2014-10-18 15:16:23 +0000

    add process

---
+++ app/controllers/web/process_controller.rb
@@ -0,0 +1,6 @@
+class Web::ProcessController < Web::ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   scope module: :web do
     root to: 'welcome#index'
 		resources :load, only: [:index, :new, :create]
+    resources :process, only: :index
   end
 
 end
