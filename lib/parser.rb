f9d03dfcd78a01ce995d0717a7c13d8360b91150
Mikhail Dronov <dronovmm@undev.ru>
2014-10-19 20:37:32 +0400

    attempt

--- lib/parser.rb
+++ lib/parser.rb
@@ -16,8 +16,25 @@ class Parser
 		file = File.read(Rails.root.join("public", "123.svg"))
 		xml = Nokogiri::XML file
 
-		atr = xml.css('//g[contains(@style,"name")]').first
-		p atr
+		svg_name = xml.xpath('//*[contains(@style,"svg_nick")]')
+		p svg_name
+		x = svg_name.attribute('x').to_s.to_f
+		y = svg_name.attribute('y').to_s.to_f
+		height = svg_name.attribute('height').to_s.to_f
+		text = "<text x='#{x.to_s}' y='#{(y + height).to_s}' font-family='Verdana' font-size='#{(0.75 * height).to_i.to_s}' fill='blue' > Mechanic </text>"
+		p text
+
+		xml.xpath('//*[contains(@style,"svg_nick")]').each do |element|
+			element.replace(text)
+		end
+		p "after removing"
+		svg_name = xml.xpath('//*[contains(@style,"svg_nick")]')
+		p svg_name
+		p xml
+
+
+
+		File.open(Rails.root.join("public", "321.svg"), 'w') { |f| f.print(xml.to_xml) }
 	end
 
 end
